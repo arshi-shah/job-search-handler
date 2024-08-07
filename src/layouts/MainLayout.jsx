ecf3b26954092fcb1a6203cc76ee47b851d561b5
arshi-shah <arshi.shah.cs@gmail.com>
2024-08-07 15:35:31 +0530

    first commit

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,18 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Outlet } from 'react-router-dom';
+import NavBar from '../components/NavBar';
+
+const MainLayout = () => {
+  return (
+    <>
+    <NavBar />
+    
+    <Outlet />
+    <ToastContainer />
+    </>
+    
+  )
+}
+
+export default MainLayout
